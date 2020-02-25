5d9cb94b956f453c46f2b5f30ce640a2ec1770c2
hafizrahman <hfzrhmn@gmail.com>
2020-02-25 15:48:46 +0700

    DB initialization.
    Plus some spacing tweak in Reminder.kt.

--- app/src/main/java/black/old/spacedrepetitionowl/database/SroDatabase.kt
+++ app/src/main/java/black/old/spacedrepetitionowl/database/SroDatabase.kt
@@ -1,11 +1,32 @@
 package black.old.spacedrepetitionowl.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import black.old.spacedrepetitionowl.models.*
 
 @Database(
-    entities = [Subject::class, Reminder::class],
-    version = 1)
+    entities    = [Subject::class, Reminder::class],
+    version     = 1)
 abstract  class SroDatabase : RoomDatabase() {
+
+    // Create the SroDatabase as a singleton to prevent having multiple instances of
+    // the database opened at the same time.
+    private var instance: SroDatabase? = null
+    fun getInstance(context: Context) : SroDatabase? {
+        if(instance == null) {
+            synchronized(SroDatabase::class) {
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    SroDatabase::class.java,
+                    "sro_database")
+                    // Wipes and rebuilds instead of migrating
+                    // if no Migration object
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }
+        }
+        return instance
+    }
 }
--- app/src/main/java/black/old/spacedrepetitionowl/models/Reminder.kt
+++ app/src/main/java/black/old/spacedrepetitionowl/models/Reminder.kt
@@ -11,7 +11,7 @@ import androidx.room.PrimaryKey
    - date               : Reminder date
    - checked            : This is to allow user to check a Reminder once they have done the learning
                           for that date
-                          
+
    ---
    Notes on dates: When it comes to the date data, and its saving/passing, the timestamp will always
    be on UTC timezone. The app will figure out later on the UI on how to convert this timestamp to a
