2f2818835334ae66750693d3fe2d34a57f07cadb
hafizrahman <hfzrhmn@gmail.com>
2020-02-26 10:12:36 +0700

    Rename ViewModel to make it clearer what it is meant to be used with.
    (with MainActivity, that is)

--- app/src/main/java/black/old/spacedrepetitionowl/viewmodels/MainViewModel.kt
+++ app/src/main/java/black/old/spacedrepetitionowl/viewmodels/MainViewModel.kt
@@ -8,9 +8,10 @@ import black.old.spacedrepetitionowl.models.Subject
 import black.old.spacedrepetitionowl.repositories.SroRepository
 import kotlinx.coroutines.launch
 
-class SroViewModel(application: Application) : AndroidViewModel(application) {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val sroRepository = SroRepository(application)
 
+    // TODO: This part needs some logic added to generate and insert Reminders based on Subject date
     fun insertSubject(subject: Subject) = viewModelScope.launch {
         sroRepository.insertSubject(subject)
     }
