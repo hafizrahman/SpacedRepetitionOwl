11104c035380b869a62320b93bd6509545cf5f2f
hafizrahman <hfzrhmn@gmail.com>
2020-05-21 14:56:06 +0700

    Show toast to confirm subject deletion.

--- app/src/main/java/black/old/spacedrepetitionowl/DialogDeleteAlertFragment.kt
+++ app/src/main/java/black/old/spacedrepetitionowl/DialogDeleteAlertFragment.kt
@@ -4,6 +4,7 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -14,15 +15,21 @@ class DialogDeleteAlertFragment(private val subjectToDeleteId: Long): DialogFrag
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return MaterialAlertDialogBuilder(requireContext())
             .setTitle("Delete Subject?")
-            .setMessage("Subject will be delete forever.")
+            .setMessage("Subject will be deleted forever.")
             .setPositiveButton("Delete",
                 DialogInterface.OnClickListener { dialog, id ->
 
-                    // TODO Get the viewmodel and make it do the deletion
+                    // Get the viewmodel and make it do the deletion
                     val mainViewModel = ViewModelProvider(activity!!).get(MainViewModel::class.java)
                     Log.d("LOVELIFE", "we about to delete subject id $subjectToDeleteId")
                     mainViewModel.deleteSubjectBySubjectId(subjectToDeleteId)
 
+                    // TODO Show a toaster to confirm that subject has been deleted
+                    val toast = Toast.makeText(
+                        requireContext(),
+                        "Subject deleted",
+                        Toast.LENGTH_LONG)
+                    toast.show()
 
                     // This alert will be called by the Bottom Dialog Fragment using show()
                     // By default, after clicking Delete, it will hide the alert and show
@@ -32,6 +39,7 @@ class DialogDeleteAlertFragment(private val subjectToDeleteId: Long): DialogFrag
                     // (the subject list fragment)
                     findNavController().popBackStack()
                 })
+
             .setNegativeButton("Cancel",
                 DialogInterface.OnClickListener { dialog, id ->
                     dialog.dismiss()
