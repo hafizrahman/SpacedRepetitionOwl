c2a1057c751b0363092d703cbb54402ba2b0bd75
hafizrahman <hfzrhmn@gmail.com>
2020-09-09 16:03:09 +0700

    Finalize refactoring by deleting the old view fragment and edit fragment.
    As well as modify the bottomDialog on the main subjects list view to show "View and Edit" in one single menu item.

--- app/src/main/java/black/old/spacedrepetitionowl/BottomDialogSubjectsNavigationFragment.kt
+++ app/src/main/java/black/old/spacedrepetitionowl/BottomDialogSubjectsNavigationFragment.kt
@@ -21,17 +21,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SubjectsBottomDialogFragment() : BottomSheetDialogFragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
     val args: SubjectsBottomDialogFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,12 +37,6 @@ class SubjectsBottomDialogFragment() : BottomSheetDialogFragment() {
             false)
         view.subjects_bottom_dialog_title.text = args.subjectText
 
-        val detailSubjectText = view.subjects_bottom_dialog_details
-        detailSubjectText.setOnClickListener { view ->
-            val action = SubjectsBottomDialogFragmentDirections
-                .actionSubjectsBottomDialogFragmentToDetailSubjectFragment(args.subjectId)
-            findNavController().navigate(action)
-        }
 
         val editSubjectText = view.subjects_bottom_dialog_edit
         editSubjectText.setOnClickListener { view ->
