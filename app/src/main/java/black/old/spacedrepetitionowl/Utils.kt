01c01bb9b26c2d41f082f9ceb96eef85451abadf
hafizrahman <hfzrhmn@gmail.com>
2020-09-22 11:26:47 +0700

    Helper functions to modify an epoch timestamp's hours/minutes.
    This is to support the feature to allow users to set their on notification hour, while ensuring it's on the right timezone the whole time.

--- app/src/main/java/black/old/spacedrepetitionowl/Utils.kt
+++ app/src/main/java/black/old/spacedrepetitionowl/Utils.kt
@@ -4,8 +4,11 @@ import android.app.Activity
 import android.content.Context
 import android.view.inputmethod.InputMethodManager
 import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.util.concurrent.TimeUnit
-import java.util.regex.Pattern
 
 // Function for hiding keyboard, mostly used when a keyboard is opened then user pressed back/up.
 // Usage: hideKeyboard(activity as MainActivity)
@@ -45,7 +48,7 @@ fun dayToMilliseconds(day: Int) : Long {
     return TimeUnit.DAYS.toMillis(day.toLong())
 }
 
-fun minutesFromMidnightToHourlyTime(minutes: Int) : String {
+fun minutesFromMidnightToHourlyTime(minutes: Int): String {
     val hour: Int = minutes / 60
     val minute: Int = minutes % 60
 
@@ -53,4 +56,25 @@ fun minutesFromMidnightToHourlyTime(minutes: Int) : String {
     // so that e.g it writes out the hour format as "01:05" instead of "1:5"
     // See https://stackoverflow.com/a/27857435
     return String.format("%02d:%02d", hour, minute)
+}
+
+// The two functions below are needed to get the current timestamp, then get the correct local date
+// (that matches the user's timezone), and finally strip off the hour/minute off of it, so that we
+// can add the user's preferred notification hours/minutes to it.
+// They're mostly used to allow users to set their own custom notification hour for each reminder.
+
+// Function 1 : We're using LocalDate because we want to ignore the hours and add it later on
+// based on default value or preferences.
+fun timestampToLocalDate(timestamp: Long): LocalDate {
+    val localTimezone = ZoneId.systemDefault()
+    return LocalDateTime
+        .ofInstant(Instant.ofEpochMilli(timestamp), localTimezone)
+        .toLocalDate()
+}
+
+// Function 2 : Get the epoch timestamp of a localDate at the start of the day.
+fun timestampFromLocalDateAtStartOfDay(localDate: LocalDate): Long {
+    val localTimezone = ZoneId.systemDefault()
+    val startofdayInstant = localDate.atStartOfDay(localTimezone).toInstant()
+    return startofdayInstant.toEpochMilli()
 }
